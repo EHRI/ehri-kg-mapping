34c906284be04eca0466cb6924c8ed3423a8317a
Herminio García González <herminiogg@hotmail.com>
2025-07-24 14:52:51 +0200

    Added SHACL shapes for validation as an alternative/complement to the ShEx ones.

--- src/validateAll.sh
+++ src/validateAll.sh
@@ -2,6 +2,7 @@
 
 echo "" > validationReport.txt # Clears the validation report file
 echo "" > shexCommandOutput.txt # Clears the ShEx command output file
+echo "" > shaclCommandOutput.txt # Clears the SHACL command output file
 
 if [ ! -d apache-jena ]; then
     echo "Downloading Apache Jena..."
@@ -12,12 +13,7 @@ fi
 #export _JAVA_OPTIONS="-Xmx12g"
 
 shex="apache-jena/apache-jena-5.4.0/bin/shex"
-
-countries=$($shex validate --schema shapes/1.0.2/countries.shex --data countries.ttl -m shapes/1.0.2/countries.smap 2>> shexCommandOutput.txt)
-conformant=$(echo "$countries" | grep "Status = conformant" | wc -l)
-non_conformant=$(echo "$countries" | grep "Status = nonconformant" | wc -l )
-{ echo "$countries" | grep "Status = nonconformant" || true; } >> validationReport.txt
-echo "Countries: $conformant conformant, $non_conformant non-conformant"
+shacl="apache-jena/apache-jena-5.4.0/bin/shacl"
 
 iterative_execution() {
     conformant=0
@@ -38,6 +34,31 @@ iterative_execution() {
     echo "$type_name: $conformant conformant, $non_conformant non-conformant"
 }
 
+iterative_execution_shacl() {
+    valid_files=0
+    non_conformant=0
+    type_name=$1
+    schema=$2
+    shift 2
+    for filename in $@; do
+        validation_result=$($shacl validate --shapes $schema --data $filename 2>> shaclCommandOutput.txt)
+        non_conformant_partial=$(echo "$validation_result" | grep "sh:Violation;" | wc -l)
+        if [ $non_conformant_partial -eq 0 ]; then
+            valid_files=$(($valid_files + 1))
+        fi
+        non_conformant=$(($non_conformant + non_conformant_partial))
+        echo "$validation_result" >> validationReport.txt
+        echo "Validating $filename: $non_conformant_partial errors"
+    done
+    echo "$type_name: $valid_files valid files, $non_conformant errors"
+}
+
+countries=$($shex validate --schema shapes/1.0.2/countries.shex --data countries.ttl -m shapes/1.0.2/countries.smap 2>> shexCommandOutput.txt)
+conformant=$(echo "$countries" | grep "Status = conformant" | wc -l)
+non_conformant=$(echo "$countries" | grep "Status = nonconformant" | wc -l )
+{ echo "$countries" | grep "Status = nonconformant" || true; } >> validationReport.txt
+echo "Countries: $conformant conformant, $non_conformant non-conformant"
+
 iterative_execution Institutions shapes/1.0.2/repositories.shex shapes/1.0.2/repositories.smap shexmlOutputInstitutions/institutions_*.ttl
 iterative_execution Holdings shapes/1.0.2/holdings.shex shapes/1.0.2/holdings.smap shexmlOutputHoldings/holding_*.ttl
 iterative_execution Terms shapes/1.0.2/terms.shex shapes/1.0.2/terms.smap shexmlOutputTerms/term_*.ttl
@@ -45,3 +66,15 @@ iterative_execution Camps shapes/1.0.2/camps.shex shapes/1.0.2/camps.smap shexml
 iterative_execution Ghettos shapes/1.0.2/ghettos.shex shapes/1.0.2/ghettos.smap shexmlOutputGhettos/ghettos_*.ttl
 iterative_execution People shapes/1.0.2/people.shex shapes/1.0.2/people.smap shexmlOutputPeople/people_*.ttl
 iterative_execution CB shapes/1.0.2/cb.shex shapes/1.0.2/cb.smap shexmlOutputCb/cb_*.ttl
+
+countries=$($shex validate --shapes shapes/1.0.2/countries.shex --data countries.ttl 2>> shaclCommandOutput.txt)
+non_conformant=$(echo "$validation_result" | grep "sh:Violation;" | wc -l)
+echo "Countries: $non_conformant errors"
+
+iterative_execution_shacl Institutions shapes/1.0.2/repositories.shacl.ttl shexmlOutputInstitutions/institutions_*.ttl
+iterative_execution_shacl Holdings shapes/1.0.2/holdings.shacl.ttl shexmlOutputHoldings/holding_*.ttl
+iterative_execution_shacl Terms shapes/1.0.2/terms.shacl.ttl shexmlOutputTerms/term_*.ttl
+iterative_execution_shacl Camps shapes/1.0.2/camps.shacl.ttl shexmlOutputCamps/camp_*.ttl
+iterative_execution_shacl Ghettos shapes/1.0.2/ghettos.shacl.ttl shexmlOutputGhettos/ghettos_*.ttl
+iterative_execution_shacl People shapes/1.0.2/people.shacl.ttl shexmlOutputPeople/people_*.ttl
+iterative_execution_shacl CB shapes/1.0.2/cb.shacl.ttl shexmlOutputCb/cb_*.ttl
